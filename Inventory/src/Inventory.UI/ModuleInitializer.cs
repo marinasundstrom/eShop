bec91f7c0a32802abd32cad53a9b50e301cb1293
Marina Sundström <marna.li@outlook.com>
2022-11-06 01:00:22 +0100

    Hide Sites

--- Inventory/src/Inventory.UI/ModuleInitializer.cs
+++ Inventory/src/Inventory.UI/ModuleInitializer.cs
@@ -35,6 +35,6 @@ public class ModuleInitializer : IModuleInitializer
 
         group.CreateItem("items", () => resources["Items"], MudBlazor.Icons.Material.Filled.ListAlt, "/inventory/items");
         group.CreateItem("warehouses", () => resources["Warehouses"], MudBlazor.Icons.Material.Filled.Warehouse, "/inventory/warehouses");
-        group.CreateItem("sites", () => resources["Sites"], MudBlazor.Icons.Material.Filled.LocationCity, "/inventory/sites");
+        //group.CreateItem("sites", () => resources["Sites"], MudBlazor.Icons.Material.Filled.LocationCity, "/inventory/sites");
     }
 }
